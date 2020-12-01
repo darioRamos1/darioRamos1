fedfc7aa7b8b756bb084ede42633da662e2aef66
Carlos Baquero <karlosmario0123@gmail.com>
2020-12-01 17:34:36 -0500

    prometheus prueba 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,12 @@ import { AppService } from './app.service';
 import { StudentsModule } from './Modules/students.module';
 import { TeachersModule } from './Modules/teachers.module';
 import { ClassgroupsModule } from './Modules/classgroup.module';
+import {PrometheusModule} from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [StudentsModule,TeachersModule,ClassgroupsModule, MongooseModule.forRoot(
+  imports: [StudentsModule,TeachersModule,ClassgroupsModule,
+  PrometheusModule.register(),
+   MongooseModule.forRoot(
     'mongodb+srv://user1:qweuio123789@cluster0.2wx5w.mongodb.net/disDB?retryWrites=true&w=majority'
     )],
   controllers: [AppController],
